8879235983666ef815ca430a61f021f61e40e9fb
daviddimic <daviddimic@hotmail.com>
2022-10-27 11:02:59 +0200

    Name changed

--- src/david/RootClass.java
+++ src/david/RootClass.java
@@ -2,6 +2,6 @@ package david;
 
 public class RootClass {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world David");
     }
 }
