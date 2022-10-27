ad05172d7df8c21619039b0087df90b48b1983bb
jovanazivkovic99 <jovanakipar@gmail.com>
2022-10-27 13:44:00 +0200

    NajvecaRazlika2 commit

--- src/zivkovicj/zadaci/NajvecaRazlika2.java
+++ src/zivkovicj/zadaci/NajvecaRazlika2.java
@@ -1,21 +1,32 @@
 package zivkovicj.zadaci;
 
+import java.util.Scanner;
+
 public class NajvecaRazlika2 {
     public static void main(String[] args) {
 
-        int[] array = {1, 2,8,3,5};
+        //int[] array = {1,2,3,9,0,8,5};
 
-        int max = 0;
-        int min = array[0];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Koliko brojeva hocete da unesete?");
+        int number = sc.nextInt();
+        int[] array = new int[number];
+        System.out.println("Unesite brojeve:");
         for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+        }
 
-            if(array[i] > max){
-                max = array[i];
-            }
-            if(array[i] < min){
-                min = array[i];
+        int maxRazlika = array[1] - array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i+1; j <  array.length; j++) {
+                if(array[j] - array[i] > maxRazlika){
+                    maxRazlika = array[j] - array[i];
+                }
             }
         }
-        System.out.println(min + " " + max);
+
+        System.out.println("Najveca razlika: " + maxRazlika);
+
     }
 }
