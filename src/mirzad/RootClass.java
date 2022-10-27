d6c85cfa4fe27dfd6fb530cbf6bbd2c86d490370
Mirzad <mirzadtahirovic00@outlook.com>
2022-10-27 11:03:39 +0200

    Proba

--- src/mirzad/RootClass.java
+++ src/mirzad/RootClass.java
@@ -2,6 +2,6 @@ package mirzad;
 
 public class RootClass {
     public static void main(String[] args) {
-        System.out.println("Hello World, Mirzad!");
+        System.out.println("Hello World, Mirzad!!");
     }
 }
