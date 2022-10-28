da7bba11c567515f5c034a0eb0bdf8eca4db072e
daviddimic <daviddimic@hotmail.com>
2022-10-28 10:39:28 +0200

    Fixed variable names

--- src/david/zadaci/nedelja02/Zadatak09.java
+++ src/david/zadaci/nedelja02/Zadatak09.java
@@ -10,38 +10,38 @@ import static david.zadaci.nedelja02.Zadatak04.inputArray;
 public class Zadatak09 {
     public static void main(String[] args) {
         try {
-            int[] array = inputArray();
-            Arrays.stream(productOfOtherElements2(array)).forEach(System.out::println);
+            int[] inputArray = inputArray();
+            Arrays.stream(productOfOtherElements2(inputArray)).forEach(System.out::println);
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
     }
 
-    private static int[] productOfOtherElements(int[] array) {
-        if (array == null) throw new NullPointerException("Null array");
+    private static int[] productOfOtherElements(int[] inputArray) {
+        if (inputArray == null) throw new NullPointerException("Null array");
 
-        int[] products = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            products[i] = product(array, i);
+        int[] products = new int[inputArray.length];
+        for (int i = 0; i < inputArray.length; i++) {
+            products[i] = product(inputArray, i);
         }
         return products;
     }
 
-    private static int product(int[] array, int i) {
-        if (array == null) throw new NullPointerException("Null array");
+    private static int product(int[] inputArray, int i) {
+        if (inputArray == null) throw new NullPointerException("Null array");
 
-        int prod = 1;
-        for (int j = 0; j < array.length; j++) {
+        int productInArray = 1;
+        for (int j = 0; j < inputArray.length; j++) {
             if (i != j)
-                prod *= array[j];
+                productInArray *= inputArray[j];
         }
-        return prod;
+        return productInArray;
     }
 
-    private static int[] productOfOtherElements2(int[] array) {
-        if (array == null) throw new NullPointerException("Null array");
+    private static int[] productOfOtherElements2(int[] inputArray) {
+        if (inputArray == null) throw new NullPointerException("Null input array");
 
-        int arrayProduct = Arrays.stream(array).reduce((a, b) -> a*b).orElse(1);
-        return  Arrays.stream(array).map(a -> arrayProduct / a).toArray();
+        int arrayProduct = Arrays.stream(inputArray).reduce((a, b) -> a*b).orElse(1);
+        return  Arrays.stream(inputArray).map(arrayElement -> arrayProduct / arrayElement).toArray();
     }
 }
