9e238d244ed04cc341e55b6349a2f6bba07cef75
kyuubi98 <tamaravucetic98@gmail.com>
2022-10-28 09:59:47 +0200

    dva najmanja broja u nizu

---
+++ src/tamara/zadaci/DvaNajmanjaBroja.java
@@ -0,0 +1,37 @@
+package tamara.zadaci;
+
+public class DvaNajmanjaBroja {
+    public static void main(String[] args) {
+        int[] niz = {5, 4, 2, -7, 0};
+
+        if(niz.length == 0) {
+            System.out.println("Niz je prazan. ");
+            return;
+        }
+
+        if(niz.length == 1) {
+            System.out.println("Niz ima samo jedan element.");
+            return;
+        }
+
+        int minEl = niz[0];
+        int minEl2 = niz[0];
+        int index = 0;
+
+        for (int i = 0; i < niz.length; i++) {
+            if (niz[i] < minEl) {
+                minEl = niz[i];
+                index = i;
+            }
+        }
+
+        for (int i = 0; i < niz.length; i++) {
+            if (niz[i] < minEl2 && index != i) {
+                minEl2 = niz[i];
+            }
+        }
+
+        System.out.println(minEl + " " + minEl2);
+
+    }
+}
