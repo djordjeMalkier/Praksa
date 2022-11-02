a558b1daf8bb46c9c1fda2fc41322fc03d7c095b
Mirzad <mirzadtahirovic00@outlook.com>
2022-11-02 10:48:48 +0100

    Refactored

--- src/mirzad/zadaci/PermutacijaLambda.java
+++ src/mirzad/zadaci/PermutacijaLambda.java
@@ -1,9 +1,7 @@
 package mirzad.zadaci;
 
 import java.util.Map;
-import java.util.Scanner;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+import java.util.Scanner;import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class PermutacijaLambda {
@@ -12,6 +10,7 @@ public class PermutacijaLambda {
         String inputString = "";
 
         Scanner scanner = new Scanner(System.in);
+
         if(scanner.hasNextLine()){
            inputString = scanner.nextLine();
         } else {
@@ -19,8 +18,16 @@ public class PermutacijaLambda {
             System.exit(0);
         }
 
-        Map<Character,Integer> mapa = IntStream.range(0,permutationString.length()).boxed().collect(Collectors.toMap(permutationString.toLowerCase()::charAt, integer -> 1, (x, y) -> x+1));
-        Map<Character,Integer> mapa2 = IntStream.range(0,inputString.length()).boxed().collect(Collectors.toMap(inputString.toLowerCase()::charAt, integer -> 1, (x, y) -> x+1));
+        Map<Character,Integer> mapa = IntStream.
+                range(0,permutationString.length()).
+                boxed().
+                collect(Collectors.toMap(permutationString.toLowerCase()::charAt, integer -> 1, (x, y) -> x+1));
+
+        Map<Character,Integer> mapa2 = IntStream.
+                range(0,inputString.length())
+                .boxed().
+                collect(Collectors.toMap(inputString.toLowerCase()::charAt, integer -> 1, (x, y) -> x+1));
+
 
        if(mapa.equals(mapa2)) System.out.println("Jeste permutacija.");
        else System.out.println("Nije permutacija.");
