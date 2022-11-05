6bbee4c4b6ddb6fd816c6ffeb513b37f73d887fa
Tamara Vucetic <tamaravucetic98@gmail.com>
2022-11-05 11:47:15 +0100

    izmjene u zadatku

--- src/tamara/zadaci/NizPozitivnihINegativnihUSumu.java
+++ src/tamara/zadaci/NizPozitivnihINegativnihUSumu.java
@@ -5,57 +5,51 @@ import java.util.Scanner;
 
 public class NizPozitivnihINegativnihUSumu {
     public static void main(String[] args) throws IOException {
-        String path = "C:\\Users\\Malkier_1\\IdeaProjects\\Praksa\\src\\tamara\\zadaci\\postupak.txt";
-        FileWriter fw = new FileWriter(path, true);
+            String path = "C:\\Users\\Malkier_1\\IdeaProjects\\Praksa\\src\\tamara\\zadaci\\postupak.txt";
+            FileWriter fw = new FileWriter(path, true);
 
-        PrintWriter pw = new PrintWriter(fw);
+            Scanner ulaz = new Scanner(System.in);
 
-        Scanner ulaz = new Scanner(System.in);
+            System.out.print("Unesi broj clanova: ");
+            int brojClanova = ulaz.nextInt();
 
-        System.out.print("Unesi broj clanova: ");
-
-        int brojClanova = ulaz.nextInt();
-        int[] zadatiNiz = new int[brojClanova];
-        System.out.println("Unesi clanove: ");
-
-        for (int i = 0; i < brojClanova; i++) {
-            System.out.print("X[" + i + "]" + ": ");
-            zadatiNiz[i] = ulaz.nextInt();
-        }
-
-        int suma = 0;
-
-        for (int i = 0; i < zadatiNiz.length; i++) {
-            if (zadatiNiz[i] < 0) {
-
-                if (zadatiNiz[i] % 2 == 0) {
-                    if(i == 0) pw.print("(" + zadatiNiz[i] + ")²");
-                    else pw.print("+(" + zadatiNiz[i] + ")²");
-                    zadatiNiz[i] *= zadatiNiz[i];
-                    suma += zadatiNiz[i];
-                } else {
-                    pw.print("-(" + zadatiNiz[i] + ")²");
-                    zadatiNiz[i] *= zadatiNiz[i];
-                    suma -= zadatiNiz[i];
-                }
-            } else{
-                if (zadatiNiz[i] % 2 == 0) {
-                    suma += zadatiNiz[i];
-                    if(i == 0) pw.print(zadatiNiz[i]);
-                    else pw.print("+" + zadatiNiz[i]);
-                } else {
-                    suma -= zadatiNiz[i];
-                    pw.print("-" + zadatiNiz[i]);
-                }
+            int[] zadatiNiz = new int[brojClanova];
+            System.out.println("Unesi clanove: ");
+            for (int i = 0; i < brojClanova; i++) {
+                System.out.print("X[" + i + "]" + ": ");
+                zadatiNiz[i] = ulaz.nextInt();
             }
 
-        }
+            int suma = 0;
 
-        pw.println();
-        System.out.println(suma);
-        pw.close();
-        fw.close();
+            for (int i = 0; i < zadatiNiz.length; i++) {
+                if (zadatiNiz[i] % 2 == 0) {
+                    if(zadatiNiz[i] < 0) {
+                        if(i == 0) fw.write(" (" + zadatiNiz[i] + ")² ");
+                        else fw.write(" +(" + zadatiNiz[i] + ")²" );
+                        zadatiNiz[i] *= zadatiNiz[i];
+                    }
+                    else {
+                        if(i == 0) fw.write(zadatiNiz[i]);
+                        else fw.write(" + " + zadatiNiz[i]);
+                    }
+                    suma += zadatiNiz[i];
 
+                } else {
+                    if(zadatiNiz[i] < 0) {
+                        fw.write(" -(" + zadatiNiz[i] + ")²");
+                        zadatiNiz[i] *= zadatiNiz[i];
+                    }
+                    else fw.write(" - " + zadatiNiz[i]);
+
+                    suma -= zadatiNiz[i];
+                }
+
+            }
+            fw.write(" = " + suma);
+            fw.write("\n");
+            System.out.println(suma);
+            fw.close();
 
     }
 }
