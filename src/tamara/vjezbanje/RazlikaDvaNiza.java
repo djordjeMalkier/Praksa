b37d0277af44e7f741d76f5202d8810d037b8454
Tamara Vucetic <tamaravucetic98@gmail.com>
2022-11-05 15:42:59 +0100

    zavrsen zadatak razlika dva niza

---
+++ src/tamara/vjezbanje/RazlikaDvaNiza.java
@@ -0,0 +1,42 @@
+package tamara.vjezbanje;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//razlika dva niza
+public class RazlikaDvaNiza {
+    public static void main(String[] args) {
+        int[] zadatiNiz1 = {4, 5, 8, -3, 12, -4, 4};
+        int[] zadatiNiz2 = {10, -8, -3, 12, 12, 4};
+        List<Integer> listaPosjecenihIndeksa1 = new ArrayList<>();
+        List<Integer> listaPosjecenihIndeksa2 = new ArrayList<>();
+        List<Integer> razlika = new ArrayList<>();
+
+
+        for (int i = 0; i < zadatiNiz1.length; i++) {
+            int brojPonavljanja = 0;
+            for (int j = 0; j < zadatiNiz2.length; j++) {
+                if(zadatiNiz1[i] == zadatiNiz2[j] && !listaPosjecenihIndeksa1.contains(j) && brojPonavljanja == 0) {
+                    brojPonavljanja++;
+                    listaPosjecenihIndeksa1.add(j);
+                }
+            }
+            if(brojPonavljanja == 0)
+                razlika.add(zadatiNiz1[i]);
+        }
+
+        for (int i = 0; i < zadatiNiz2.length; i++) {
+            int brojPonavljanja = 0;
+            for (int j = 0; j < zadatiNiz1.length; j++) {
+                if(zadatiNiz2[i] == zadatiNiz1[j] && !listaPosjecenihIndeksa2.contains(j) && brojPonavljanja == 0) {
+                    brojPonavljanja++;
+                    listaPosjecenihIndeksa2.add(j);
+                }
+            }
+            if(brojPonavljanja == 0)
+                razlika.add(zadatiNiz2[i]);
+        }
+
+        System.out.println("Razlika: " + razlika);
+    }
+}
