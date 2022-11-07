b773763411b969388ce46b3f91d45f2336f0da50
jovanazivkovic99 <jovanakipar@gmail.com>
2022-11-07 11:00:26 +0100

    Chat threads dupliran

---
+++ src/zivkovicj/zadaci/threads2/ChatThread.java
@@ -0,0 +1,25 @@
+package zivkovicj.zadaci.threads2;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ChatThread implements Runnable{
+    private String name;
+
+    private FileWriter file;
+
+    public ChatThread(FileWriter file, String name){
+        this.file = file;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        try{
+            file.write(name + Thread.currentThread().getName() + "\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
