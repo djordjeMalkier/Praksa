c1d08aecc406362ccbaca8bd37d9bd75279143ef
daviddimic <daviddimic@hotmail.com>
2022-11-15 15:01:01 +0100

    init kurs

--- src/main/java/common/bankarskiSistem/utils/Constants.java
+++ src/main/java/common/bankarskiSistem/utils/Constants.java
@@ -3,8 +3,8 @@ package common.bankarskiSistem.utils;
 public class Constants {
 
     public static final String IP = "localhost:5432";
-    public static final String DATABASE = "Banka";
+    public static final String DATABASE = "banka";
     public static final String USERNAME = "postgres";
-    public static final String PASSWORD = "password";
+    public static final String PASSWORD = "admin";
 
 }
