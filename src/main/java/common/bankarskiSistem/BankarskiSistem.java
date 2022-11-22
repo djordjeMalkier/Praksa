f455f22d6124a45baa76745de77888e3b503d748
kyuubi98 <tamaravucetic98@gmail.com>
2022-11-22 13:07:17 +0100

    demo

--- src/main/java/common/bankarskiSistem/BankarskiSistem.java
+++ src/main/java/common/bankarskiSistem/BankarskiSistem.java
@@ -1,6 +1,11 @@
 package common.bankarskiSistem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BankarskiSistem {
     public static void main(String[] args) {
-
+        SpringApplication.run(BankarskiSistem.class, args);
     }
 }
