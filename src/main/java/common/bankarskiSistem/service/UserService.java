0c4384786ae41fedec3d4a3cedc006f2a4fa900a
Mirzad <mirzadtahirovic00@outlook.com>
2022-11-23 12:34:29 +0100

    Completed task with bank accounts in user service

--- src/main/java/common/bankarskiSistem/service/UserService.java
+++ src/main/java/common/bankarskiSistem/service/UserService.java
@@ -1,8 +1,13 @@
 package common.bankarskiSistem.service;
 
 import common.bankarskiSistem.model.BankAccount;
+import common.bankarskiSistem.model.User;
 import common.bankarskiSistem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class UserService {
 
@@ -13,5 +18,28 @@ public class UserService {
 
         return 0;
     }
+
+    public boolean deleteAccount(String personalId, BankAccount bankAccount){
+        Optional<User> user = userRepository.findById(personalId);
+        if(user.
+                isEmpty()) return false;
+        user.ifPresent(u -> u.getBankAccounts().remove(bankAccount));
+        user.ifPresent(u -> userRepository.save(u));
+        return true;
+    }
+
+    public boolean deleteAllAccount(String personalId){
+        Optional<User> user = userRepository.findById(personalId);
+        if(user.isEmpty()) return false;
+        user.ifPresent(u -> u.setBankAccounts(new ArrayList<BankAccount>()));
+        user.ifPresent(u -> userRepository.save(u));
+        return true;
+    }
+
+    public List<BankAccount> getAllAccounts(String personalId){
+        Optional<User> user = userRepository.findById(personalId);
+        return user.map(User::getBankAccounts).orElse(null);
+    }
+
 
 }
