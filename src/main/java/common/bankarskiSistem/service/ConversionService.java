b498fc7ced227b1483bf313b80b3d5d9807549d9
jovanazivkovic99 <jovanakipar@gmail.com>
2022-11-24 10:09:51 +0100

    addConversion

--- src/main/java/common/bankarskiSistem/service/ConversionService.java
+++ src/main/java/common/bankarskiSistem/service/ConversionService.java
@@ -3,6 +3,7 @@ package common.bankarskiSistem.service;
 import common.bankarskiSistem.model.Bank;
 import common.bankarskiSistem.model.Conversion;
 import common.bankarskiSistem.model.Currency;
+import common.bankarskiSistem.model.User;
 import common.bankarskiSistem.repository.ConversionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,5 +30,17 @@ public class ConversionService {
         Conversion conversion = conversionOptional.get();
 
         return conversion.getValue();
+    }
+
+    public Conversion addConversion(Conversion conversion){
+        if(conversion == null)
+            throw new NullPointerException("Null conversion");
+        Conversion existingConversion
+                = conversionRepository.findById(conversion.getIdConversion())
+                .orElse(null);
+        if (existingConversion == null)
+            return conversionRepository.save(conversion);
+
+        throw new NullPointerException("This conversion already exists!");
     }
 }
