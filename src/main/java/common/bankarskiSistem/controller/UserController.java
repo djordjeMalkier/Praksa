732494e0fb0143ade5f849c3665e12755c744b4b
Mirzad <mirzadtahirovic00@outlook.com>
2022-11-28 10:46:44 +0100

    Add methods for account in controller

--- src/main/java/common/bankarskiSistem/controller/UserController.java
+++ src/main/java/common/bankarskiSistem/controller/UserController.java
@@ -2,8 +2,11 @@ package common.bankarskiSistem.controller;
 
 
 import common.bankarskiSistem.BankarskiSistem;
+import common.bankarskiSistem.controller.dto.BankAccountDTO;
+import common.bankarskiSistem.controller.dto.BankAccountMapper;
 import common.bankarskiSistem.controller.dto.UserDTO;
 import common.bankarskiSistem.controller.dto.UserMapper;
+import common.bankarskiSistem.model.BankAccount;
 import common.bankarskiSistem.model.User;
 import common.bankarskiSistem.service.UserService;
 import org.slf4j.Logger;
@@ -11,6 +14,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
 
 import static org.springframework.http.ResponseEntity.*;
 
@@ -22,6 +28,7 @@ public class UserController {
     private UserService userService;
 
     private final UserMapper mapper = UserMapper.INSTANCE;
+    private final BankAccountMapper bankAccountMapper = BankAccountMapper.INSTANCE;
 
 
     @PostMapping
@@ -49,36 +56,67 @@ public class UserController {
         }
     }
 
-    @PostMapping
-    public ResponseEntity<UserDTO> deleteAccount(@RequestBody UserDTO userDto) {
-        User savedUser = null;
+    @PostMapping("/{personal_id}/accounts/{bankAccountID}/delete")
+    public ResponseEntity<BankAccountDTO> deleteAccount(
+            @PathVariable String personal_id,
+            @PathVariable String bankAccountID
+    ) {
+        BankAccount bankAccount = null;
         try {
-            User user = mapper.userDTOtoUser(userDto);
-            savedUser = userService.saveUser(user);
+            bankAccount = userService.getBankAccountByID(personal_id, Integer.getInteger(bankAccountID));
+            User user = bankAccount.getUser();
+            userService.deleteAccount(user.getPersonalId(), bankAccount);
 
         } catch (NullPointerException exception) {
             return badRequest().build();
         }
-        return ok(mapper.userToUserDTO(savedUser));
+        return ok(bankAccountMapper.convertToDTO(bankAccount));
     }
 
-    /*
-    * updateUser
-    * deleteUserByPersonalId
-    * createBankAccountForUser
-    *
-    * payInUserBankAccount
-    * payOutUserBankAccount
-    * transferFromUserAccountToAnotherAccount
-    * getAccountBalance
-    * getAllBankAccountBalance
-    *
-    * deleteOneAccount
-    * deleteAllAccount
-    * getAllAccounts
-    * getBankAccountById
-    *
-    * */
+    @PostMapping("/{personal_id}/accounts/delete")
+    public ResponseEntity<UserDTO> deleteAllAccounts(@PathVariable String personal_id) {
+        User user = null;
+        try {
+            user = userService.getUserByPersonalID(personal_id);
+            userService.deleteAllAccounts(user.getPersonalId());
 
+        } catch (NullPointerException exception) {
+            return badRequest().build();
+        }
+        return ok(mapper.userToUserDTO(user));
+    }
+
+    @GetMapping("/{personal_id}/accounts")
+    public ResponseEntity<List<BankAccountDTO>> getAllAccounts(@PathVariable String personal_id) {
+        User user = null;
+        try{
+            user = userService.getUserByPersonalID(personal_id);
+            log.info(user.toString());
+            return ok(bankAccountMapper.bankAccountsToDTO(user.getBankAccounts()));
+        } catch (NullPointerException exception) {
+            return notFound().build();
+        }
+    }
+
+    @GetMapping("{personal_id}/accounts/{account_id}")
+    public ResponseEntity<BankAccountDTO> getAccount(
+            @PathVariable String personal_id,
+            @PathVariable String account_id
+    ) {
+        User user = null;
+        try{
+            user = userService.getUserByPersonalID(personal_id);
+            log.info(user.toString());
+
+            List<BankAccount> bankAccounts = user.getBankAccounts();
+            for(BankAccount account : bankAccounts){
+                if (Objects.equals(account.getIdAccount(), Integer.getInteger(account_id)))
+                    return ok(bankAccountMapper.convertToDTO(account));
+            }
+        } catch (NullPointerException exception) {
+            return notFound().build();
+        }
+        return null;
+    }
 
 }
--- src/main/java/common/bankarskiSistem/controller/dto/BankAccountMapper.java
+++ src/main/java/common/bankarskiSistem/controller/dto/BankAccountMapper.java
@@ -6,6 +6,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper
 public interface BankAccountMapper {
 
@@ -21,5 +23,7 @@ public interface BankAccountMapper {
 
     @InheritInverseConfiguration
     BankAccount convertToEntity(BankAccountDTO bankAccountDTO);
+
+    List<BankAccountDTO> bankAccountsToDTO(List<BankAccount> bankAccounts);
 
 }
