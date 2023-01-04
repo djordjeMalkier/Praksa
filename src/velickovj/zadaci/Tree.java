f73e1e1b9e0955652c25dc846d2eba521a359d7a
jovana-velickov <jovana.g.velickov@gmail.com>
2023-01-04 14:48:56 +0100

    izmenjen ispis stabla

--- src/velickovj/zadaci/Tree.java
+++ src/velickovj/zadaci/Tree.java
@@ -1,8 +1,60 @@
 package velickovj.zadaci;
 
+class Trunk
+{
+    Trunk prev;
+    String str;
 
+    Trunk(Trunk prev, String str)
+    {
+        this.prev = prev;
+        this.str = str;
+    }
+};
 
 public class Tree {
+    public static void showTrunks(Trunk p)
+    {
+        if (p == null) {
+            return;
+        }
+        showTrunks(p.prev);
+        System.out.print(p.str);
+    }
+
+    public static void printTree(Node root, Trunk prev, boolean isLeft)
+    {
+        if (root == null) {
+            return;
+        }
+
+        String prev_str = "    ";
+        Trunk trunk = new Trunk(prev, prev_str);
+
+        printTree(root.getRight(), trunk, true);
+
+        if (prev == null) {
+            trunk.str = "———";
+        }
+        else if (isLeft) {
+            trunk.str = ".———";
+            prev_str = "   |";
+        }
+        else {
+            trunk.str = "`———";
+            prev.str = prev_str;
+        }
+
+        showTrunks(trunk);
+        System.out.println(" " + root.getKey());
+
+        if (prev != null) {
+            prev.str = prev_str;
+        }
+        trunk.str = "   |";
+
+        printTree(root.getLeft(), trunk, false);
+    }
 
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
@@ -15,9 +67,9 @@ public class Tree {
         tree.insert(10);
         tree.insert(14);
         tree.insert(4);
-        System.out.println(tree.getRoot());
         tree.printTree(tree.getRoot());
         System.out.println();
+       printTree(tree.getRoot(),null,false);
 
 
         BalancedBinaryTree tree1 = new BalancedBinaryTree();
@@ -31,6 +83,8 @@ public class Tree {
         tree1.setNode(tree1.insertNode(tree1.getNode(), 8));
         tree1.setNode(tree1.insertNode(tree1.getNode(), 11));
         tree1.printTree(tree1.getNode());
+        System.out.println();
+        printTree(tree1.getNode(),null,false);
 
     }
 
